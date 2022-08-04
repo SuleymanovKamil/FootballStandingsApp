c77697d11cf51707654075be3803ceef4605d9ce
Kamil Suleimanov <38867359+krakatoor@users.noreply.github.com>
2022-08-04 17:30:14 +0300

    refactoring

--- FootballStandingsApp/Core/Screens/League Season Detail View/Views/Season Rank/SeasonRank.swift
+++ FootballStandingsApp/Core/Screens/League Season Detail View/Views/Season Rank/SeasonRank.swift
@@ -13,64 +13,8 @@ struct SeasonRank: View {
     
     var body: some View {
         VStack (alignment: .leading, spacing: 0) {
-            if let seasons = store.seasons?.seasons {
-                VStack(alignment: .leading) {
-                    Text("Seasons:")
-                        .font(.headline)
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack {
-                            ForEach(seasons, id:\.year) { season in
-                                Button {
-                                    Task {
-                                        store.status = .loading
-                                        await store.fetchSeason(id: store.league.id, year: season.year)
-                                    }
-                                } label: {
-                                    Text(String(season.year))
-                                        .padding(.horizontal)
-                                        .padding(.vertical, 3)
-                                        .background((season.year == store.season?.season ? Color.black.opacity(0.06) :  Color.white).cornerRadius(4))
-                                }
-                            }
-                        }
-                        
-                    }
-                }
-                .padding(.vertical)
-                .background(Color.white)
-            }
-            
-            
-            if let season = store.season {
-                Text("Table:")
-                    .font(.headline)
-                    .padding(.bottom)
-                
-                ForEach(0..<season.standings.count, id: \.self) { index in
-                    let standing = season.standings[index]
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Text("\(index + 1)")
-                            
-                            if let logoURL = standing.team?.logos?.first?.href {
-                                AsyncImageView(url: logoURL, width: 25, height: 25)
-                            }
-                            
-                            Text(standing.team?.name ?? "")
-                            
-                            Spacer()
-                            
-                            let point = standing.stats.filter({$0.name == "points"})
-                            if let value = point.first?.value {
-                                Text("\(value)")
-                            }
-                        }
-                    }
-                    .padding()
-                    .background(index % 2 == 0 ? Color.black.opacity(0.06) : Color.white)
-                }
-            }
+            seasonsScrollView
+            seasonsRank
         }
         .padding()
     }
@@ -80,5 +24,67 @@ struct SeasonRank_Previews: PreviewProvider {
     static var previews: some View {
         let presenter = AllLeaguesMainViewPresenter(footballStandingsService: FootballStandingsRequest())
         AllLeaguesMainView(store:presenter, presenter: presenter)
+    }
+}
+
+extension SeasonRank {
+    @ViewBuilder private var seasonsScrollView: some View {
+        if let seasons = store.seasons?.seasons {
+            VStack(alignment: .leading) {
+                Text("All Seasons:")
+                    .font(.headline)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(seasons, id:\.year) { season in
+                            Button {
+                                Task {
+                                    store.status = .loading
+                                    await store.fetchSeason(id: store.league.id, year: season.year)
+                                }
+                            } label: {
+                                Text(String(season.year))
+                                    .padding(.horizontal)
+                                    .padding(.vertical, 3)
+                                    .background((season.year == store.season?.season ? Color.black.opacity(0.06) :  Color.white).cornerRadius(4))
+                            }
+                        }
+                    }
+                }
+            }
+            .padding(.vertical)
+            .background(Color.white)
+        }
+    }
+    @ViewBuilder private var seasonsRank: some View {
+        if let season = store.season {
+            Text("Table:")
+                .font(.headline)
+                .padding(.bottom)
+            
+            ForEach(0..<season.standings.count, id: \.self) { index in
+                let standing = season.standings[index]
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("\(index + 1)")
+                        
+                        if let logoURL = standing.team?.logos?.first?.href {
+                            AsyncImageView(url: logoURL, width: 25, height: 25)
+                        }
+                        
+                        Text(standing.team?.name ?? "")
+                        
+                        Spacer()
+                        
+                        let point = standing.stats.filter({$0.name == "points"})
+                        if let value = point.first?.value {
+                            Text("\(value)")
+                        }
+                    }
+                }
+                .padding()
+                .background(index % 2 == 0 ? Color.black.opacity(0.06) : Color.white)
+            }
+        }
     }
 }
